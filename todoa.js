fd72811a9f7401b55455ed252389f5b7f5fcdf84
tanje01 <tanje01@luther.edu>
2015-10-16 14:28:08 -0500

    Create todoa.js

---
+++ todoa.js
@@ -0,0 +1,18 @@
+add = function() {
+   var newTask;
+   var list;
+   var checkbox;
+   var contents;
+   newTask = document.createElement("li");
+   checkbox = document.createElement("input");
+   checkbox.type("checkbox");
+   newTask.appendChild(checkbox);
+   
+   
+   list = document.querySelector('#todobox');
+   list.innerHTML=list
+
+
+
+
+}
