8f3765aff13ea6d8f6b6fef61964ff93f21859cf
tanje01 <tanje01@luther.edu>
2015-10-19 13:37:33 -0500

    Update todoa.js

--- todoa.js
+++ todoa.js
@@ -3,14 +3,23 @@ add = function() {
    var list;
    var checkbox;
    var contents;
+   var words;
+   var priority;
+   priority = document.querySelector("#priority").value;   
+   console.log(priority);
    newTask = document.createElement("li");
    checkbox = document.createElement("input");
-   checkbox.type("checkbox");
+   checkbox.type = "checkbox";
    newTask.appendChild(checkbox);
-   
-   
-   list = document.querySelector('#todobox');
-   list.innerHTML=list
+   words = document.querySelector("#enterHere").value;
+   contents = document.createTextNode(words);
+   newTask.className = priority;
+   newTask.appendChild(contents);
+   console.log(newTask.className);
+   list = document.querySelector('ul');
+   list.appendChild(newTask);
+}
+finished = function() {
 
 
 
